b43c9f377276d6434bf7954f9cef31bf18a95b5b
DESKTOP-IIJ3FV6\KJH <rkdwo321@gmail.com>
2024-04-04 10:26:18 +0900

    테스트

---
+++ lib/src/main/java/loaSSalmuckBot/com/api/test/TestController.java
@@ -0,0 +1,29 @@
+package loaSSalmuckBot.com.api.test;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import loaSSalmuckBot.com.api.jpa.channel.VoiceChannelEntity;
+import loaSSalmuckBot.com.api.jpa.channel.VoiceChannelRepository;
+
+@Controller("/api/v1/test")
+public class TestController {
+
+	@Autowired
+	private VoiceChannelRepository repository;
+
+	@GetMapping("/{id}")
+	public VoiceChannelEntity getVoiceChannel(@RequestParam("id") Long id) {
+		return repository.findById(id).get();
+	}
+	
+	@GetMapping()
+	public List<VoiceChannelEntity> getVoiceChannel() {
+		return repository.findAll();
+	}
+
+}
