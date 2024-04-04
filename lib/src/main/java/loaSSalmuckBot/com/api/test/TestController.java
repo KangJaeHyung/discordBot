150483898003620db7310dbe032c189e51e60c72
DESKTOP-IIJ3FV6\KJH <rkdwo321@gmail.com>
2024-04-04 12:17:37 +0900

    no message

--- lib/src/main/java/loaSSalmuckBot/com/api/test/TestController.java
+++ lib/src/main/java/loaSSalmuckBot/com/api/test/TestController.java
@@ -3,10 +3,9 @@ package loaSSalmuckBot.com.api.test;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import loaSSalmuckBot.com.api.jpa.channel.VoiceChannelEntity;
@@ -20,7 +19,7 @@ public class TestController {
 	private VoiceChannelRepository repository;
 
 	@GetMapping("/{id}")
-	public VoiceChannelEntity getVoiceChannel(@RequestParam("id") Long id) {
+	public VoiceChannelEntity getVoiceChannel(@PathVariable("id") Long id) {
 		return repository.findById(id).get();
 	}
 	
