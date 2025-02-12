fac6fb65888e274ee17452527a83a298db0071e5
kang <rkdwo321@gmail.com>
2025-02-12 11:19:06 +0900

    no message

--- lib/src/main/java/loaSSalmuckBot/com/api/jpa/channel/MsgIdTableRepository.java
+++ lib/src/main/java/loaSSalmuckBot/com/api/jpa/channel/MsgIdTableRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import loaSSalmuckBot.com.Listener.dto.Given;
 
-public interface MsgIdTableRepository extends JpaRepository<MsgIdTableEntity, Long>{
+public interface MsgIdTableRepository extends JpaRepository<MsgIdTableEntity, String>{
 	public MsgIdTableEntity findByChannelId(String channelId);
 }
